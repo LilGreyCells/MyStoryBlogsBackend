8059c1a0ad95b6f6046d5b395bd08ad1dfc3f259
Niharika <niharika13dalal@gmail.com>
2020-06-17 19:52:02 -0400

    added origin to cors

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors({ credentials: true }))
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter)
